60a7f7712933196d71ce6554afec1a64051aaa3e
Mike Sukmanowsky <mike.sukmanowsky@gmail.com>
2020-09-09 17:45:03 -0400

    Forgot to move admin

--- naics_scian/table/admin.py
+++ naics_scian/table/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from naics_scian.table.models import NAICSClassification
 
-# Register your models here.
+
+@admin.register(NAICSClassification)
+class NAICSClassificationAdmin(admin.ModelAdmin):
+    list_display = (
+        "code",
+        "structure",
+        "class_title",
+    )
+    readonly_fields = (
+        "level",
+        "structure",
+        "code",
+        "class_title",
+        "superscript",
+        "class_definition",
+        "parent",
+    )
+    list_filter = (
+        "level",
+        "structure",
+    )
+    search_fields = (
+        "class_title",
+        "class_definition",
+    )
+
+    def has_add_permission(self, request, obj=None):
+        return False
