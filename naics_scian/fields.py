a2408c63874d1c645b9f6556f8f11137a60fec7c
Mike Sukmanowsky <mike.sukmanowsky@gmail.com>
2020-09-09 18:03:44 -0400

    Add separate fields for sector and industry

--- naics_scian/fields.py
+++ naics_scian/fields.py
@@ -2,20 +2,55 @@ from django.db import models
 
 from naics_scian.data import read_naics_csv
 
-CHOICES = [
-    (
-        row.code,
-        f"{row.code}: {row.structure} / {row.class_title}"
-        + (f" ({row.superscript})" if row.superscript else ""),
-    )
-    for row in read_naics_csv()
-]
+classifications = list(read_naics_csv())
+
+
+def make_choices(classifications):
+    return [
+        (
+            row.code,
+            f"{row.code}: {row.structure} / {row.class_title}"
+            + (f" ({row.superscript})" if row.superscript else ""),
+        )
+        for row in classifications
+    ]
+
+
+ALL_CODE_CHOICES = make_choices(classifications)
+
+SECTOR_CODE_CHOICES = make_choices([c for c in classifications if c.structure == "Sector"])
+
+INDUSTRY_CODE_CHOICES = make_choices(
+    [
+        c
+        for c in classifications
+        if c.structure == "Industry" or c.structure == "Canadian industry"
+    ]
+)
 
 
 class NAICSCodeField(models.CharField):
     def __init__(self, *args, **kwargs):
         kwargs["max_length"] = 10
-        kwargs["choices"] = CHOICES
+        kwargs["choices"] = ALL_CODE_CHOICES
+        kwargs.setdefault("blank", True)
+        kwargs.setdefault("null", True)
+        super().__init__(*args, **kwargs)
+
+
+class NAICSSectorCodeField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs["max_length"] = 10
+        kwargs["choices"] = SECTOR_CHOICES
+        kwargs.setdefault("blank", True)
+        kwargs.setdefault("null", True)
+        super().__init__(*args, **kwargs)
+
+
+class NAICSIndustryCodeField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs["max_length"] = 10
+        kwargs["choices"] = INDUSTRY_CODE_CHOICES
         kwargs.setdefault("blank", True)
         kwargs.setdefault("null", True)
         super().__init__(*args, **kwargs)
